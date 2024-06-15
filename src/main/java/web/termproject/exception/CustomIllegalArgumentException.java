24ba8cefe7eedda3b6d67c1b62569a455e58316f
Jihyun3478 <leejihyun3478@gmail.com>
2024-06-16 00:06:01 +0900

    fix: 엔티티 수정

--- src/main/java/web/termproject/exception/CustomIllegalArgumentException.java
+++
@@ -1,9 +0,0 @@
-package web.termproject.exception;
-
-public class CustomIllegalArgumentException extends IllegalArgumentException {
-    public CustomIllegalArgumentException(ErrorCode errorCode) {
-    }
-
-    public CustomIllegalArgumentException(ErrorCode errorCode, String message) {
-    }
-}
