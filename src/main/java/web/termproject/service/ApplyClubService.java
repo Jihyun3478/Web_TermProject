3e4bb0ed7b67bfc5b551e0adb9476c1acddaec5d
Jihyun3478 <leejihyun3478@gmail.com>
2024-06-19 15:20:31 +0900

    feat: 더미데이터 추가

--- src/main/java/web/termproject/service/ApplyClubService.java
+++ src/main/java/web/termproject/service/ApplyClubService.java
@@ -4,15 +4,18 @@ import web.termproject.domain.dto.request.ApplyClubRequestDTO;
 import web.termproject.domain.dto.response.ApplyClubResponseDTO;
 import web.termproject.domain.dto.response.ClubResponseDTO;
 import web.termproject.domain.entity.ApplyClub;
+import web.termproject.domain.status.ApplyClubStatus;
 
 import java.util.List;
 
 public interface ApplyClubService {
     ApplyClubResponseDTO createApplyClub(ApplyClubRequestDTO requestDTO);
+    ApplyClub createApplyClubEntity(ApplyClubRequestDTO requestDTO);
     ClubResponseDTO createClub(ApplyClub applyClub);
     ApplyClubResponseDTO refuseClub(Long applyClubId, String refuseReason);
     List<ApplyClubResponseDTO> findAll();
     List<ApplyClubResponseDTO> findApplyClubByMember(String loginId);
     ApplyClub findById(Long id);
     void save(ApplyClub applyClub);
+    List<ApplyClub> findByApplyClubStatus(ApplyClubStatus applyClubStatus);
 }
